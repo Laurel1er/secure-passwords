9d7cbc8209a864587696bd1d74df236b52641d61
Aurélien Dame <aurelien.dame@outlook.be>
2024-11-10 10:04:13 +0000

    Ajout de la séquence (HashSet)

--- CheckPassword.java
+++ CheckPassword.java
@@ -1,17 +1,21 @@
+import java.util.HashSet;
 import java.util.Scanner;
 
 public class CheckPassword {
 
     public static void main(String[] args) {
         
-        String regexSequence = "";
+        HashSet<String> sequence = new HashSet<String>();
+        sequence.add("1234");
+        sequence.add("azerty");
+        sequence.add("qwerty");
         String regexRepeated = "[a-zA-Z0-9]{3,}";
         Scanner inScanner = new Scanner(System.in);
         System.out.print("Enter your password: ");
         
         String password = inScanner.nextLine();
 
-        if (password.matches(regexSequence)) {
+        if (password.matches(regexRepeated) || password.length() < 8 || password.length() > 32 || sequence.contains(password)) {
             System.out.println("Password too esay to guess!");
         }
 
